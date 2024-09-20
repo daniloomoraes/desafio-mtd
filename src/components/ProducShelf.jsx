9d73ee766a0af31c51cb7d36fe838478fc14f8d8
Danilo Moraes <daniloomoraes@gmail.com>
2024-09-20 11:17:02 -0300

    style: ajustes shelfs

--- src/components/ProducShelf.jsx
+++ src/components/ProducShelf.jsx
@@ -3,7 +3,7 @@ import { useCookies } from "react-cookie";
 
 function ProductShelf({ item }) {
   const [isHovered, setIsHovered] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(0);
   const [cookies, setCookie] = useCookies(["myCart"]);
 
   useEffect(() => {
@@ -69,32 +69,60 @@ function ProductShelf({ item }) {
   };
 
   return (
-    <li key={item.name} className="col-span-1">
-      <img src={item.image.mobile} alt={item.name} className="hidden w-full" />
-      <img src={item.image.desktop} alt={item.name} className="block w-full" />
+    <li key={item.name} className="col-span-1 mb-5">
+      <img
+        src={item.image.mobile}
+        alt={item.name}
+        className={`block md:hidden w-full rounded-md border-2 ${
+          isHovered ? "border-[#C83B0E]" : "border-transparent"
+        }`}
+      />
+      <img
+        src={item.image.desktop}
+        alt={item.name}
+        className={`hidden md:block w-full rounded-md border-2 ${
+          isHovered ? "border-[#C83B0E]" : "border-transparent"
+        }`}
+      />
       <div
-        onClick={handleMouseEnter}
+        onMouseOver={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        className="relative"
+        className="relative flex justify-center mt-[-20px] mb-4"
       >
-        <button onClick={addToCart}>Add to Cart</button>
+        <button
+          onClick={addToCart}
+          className="border-2 border-black rounded-3xl flex items-center font-bold justify-center gap-3 py-2 w-full bg-white max-w-[170px]"
+        >
+          <img src="/assets/images/icon-add-to-cart.svg" alt="Add to Cart" />
+          Add to Cart
+        </button>
         {isHovered && (
-          <div className="absolute bg-white w-full top-0">
-            <button onClick={decreaseQuantity}>-</button>
-            <input
-              type="number"
-              value={quantity}
-              onChange={handleQuantityChange}
-              style={{ width: "40px", textAlign: "center" }}
-            />
-            <button onClick={increaseQuantity}>+</button>
+          <div className="absolute bg-[#C83B0E] border-2 border-[#C83B0E] max-w-[170px] h-[44px] rounded-3xl w-full top-0 flex justify-between items-center px-3">
+            <button
+              onClick={decreaseQuantity}
+              className="border-2 border-white rounded-2xl w-[20px] h-[20px] flex justify-center items-center"
+            >
+              <img
+                src="/assets/images/icon-decrement-quantity.svg"
+                alt="Decrement Quantity"
+              />
+            </button>
+            <span className="text-white">{quantity}</span>
+            <button
+              onClick={increaseQuantity}
+              className="border-2 border-white rounded-2xl w-[20px] h-[20px] flex justify-center items-center"
+            >
+              <img
+                src="/assets/images/icon-increment-quantity.svg"
+                alt="Increment Quantity"
+              />
+            </button>
           </div>
         )}
       </div>
-      <p>${item.price.toFixed(2)}</p>
-      <p>{item.category}</p>
-      <p>{item.name}</p>
-      <hr />
+      <p className="text-sm text-gray-500 mb-2">{item.category}</p>
+      <p className="font-bold  mb-2">{item.name}</p>
+      <p className="font-bold text-[#C83B0E]  mb-2">${item.price.toFixed(2)}</p>
     </li>
   );
 }
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,18 +5,20 @@ import MiniCart from "../components/MiniCart";
 export default function Root() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-[2vw]">
-        <div className="col-span-2">
-          <h1>Desserts</h1>
-          <ul className="grid grid-cols-3 gap-5">
-            {products.map((item) => (
-              <ProducShelf item={item} />
-            ))}
-          </ul>
-        </div>
-        <div className="col-span-1">
-          <div>
-            <MiniCart />
+      <div className=" bg-[#FCF8F5]">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-[2vw] w-full max-w-[1200px] m-auto p-5 md:p-10">
+          <div className="col-span-2">
+            <h1 className="py-5 md:py-10 font-bold text-2xl md:text-4xl">Desserts</h1>
+            <ul className="grid grid-cols-1 md:grid-cols-3 gap-5">
+              {products.map((item) => (
+                <ProducShelf item={item} />
+              ))}
+            </ul>
+          </div>
+          <div className="col-span-1">
+            <div>
+              <MiniCart />
+            </div>
           </div>
         </div>
       </div>
